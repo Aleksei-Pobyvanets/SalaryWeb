932ba1782fea59d1893495927f414d73a9077bdf
Aleksei <kilroygood@gmail.com>
2022-04-26 14:49:03 +0300

    add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
 
+
+
+  const [walletAddr, satWalletAddr] = useState("")
+
   async function reqestAcc() {
     console.log("reqesting account")
 
+    async function connectWallet() {
+        if(typeof window.ethereum != "udefined"){
+          await reqestAcc();
+
+          const provider = new ethers.providers.Web3Provider(window.ethereum)
+        }
+    }
+
     if(window.ethereum) {
       console.log("desited")
-
-      try{
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        console.log(accounts)
-      } catch (error) {
-        console.log("error cant connect")
-      }
-
+        try{
+          const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+          });
+          satWalletAddr(accounts[0]);
+        } catch (error) {
+          console.log("error cant connect")
+        }
     } else {
       console.log("Metamask not desited")
     }
@@ -28,7 +40,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <button onClick={reqestAcc}>Connect</button>
-        <h3>Walet Address ....</h3>
+        <h3>Walet Address {walletAddr}</h3>
       </header>
     </div>
   );
