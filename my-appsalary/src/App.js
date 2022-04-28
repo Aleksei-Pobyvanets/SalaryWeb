f07ec849a3db0eb3125666bea1e4304d92130107
Aleksei <kilroygood@gmail.com>
2022-04-28 13:53:40 +0300

    add

--- my-appsalary/src/App.js
+++ my-appsalary/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       
+      <button onClick={async () => {
+        alert("Connected");
+        if(window.ethereum){
+            
+        }
+      }}>
+
+      </button>
+
       </header>
     </div>
   );
