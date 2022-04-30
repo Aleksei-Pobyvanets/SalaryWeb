90bfdc89994aeed044f706e09a21b0c50e25a751
Aleksei <kilroygood@gmail.com>
2022-04-30 23:09:21 +0300

    add

--- salaryapp/src/App.js
+++ salaryapp/src/App.js
@@ -1,34 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 import { ethers } from 'ethers';
+import { useState } from 'react';
 
 function App() {
 
-  async function requestAcc(){
-    console.log("Connected");
+  const [walletAddress, setWalletAddress] = useState("");
+
+  // async function requestAcc(){
+  //   console.log("Connected");
     
+  //   if(window.ethereum){
+  //     console.log("Yes");
+
+  //     try {
+  //       const accounts = await window.ethereum.request({
+  //         method: "eth_requestAccounts",
+  //       })
+  //       setWalletAddress(accounts[0])
+  //     } catch (error) {
+  //       console.log("error connecting")
+  //     }
+
+  //   } else {
+  //     console.log("not detected");
+  //   }
+  // }
+
+  async function connectWallet() {
     if(window.ethereum){
-      console.log("Yes");
-
-      try {
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        })
-        console.log(accounts)
-      } catch (error) {
-        console.log("error connecting")
-      }
-
-    } else {
-      console.log("not detected");
+      window.ethereum.request({method: "eth_requestAccounts"});
     }
+    // if(typeof window.ethereum !== 'undefiend'){
+    //   await requestAcc();
+
+    //   const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // }
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={requestAcc}>Metamask</button>
-        <h3>Connected</h3>
+        <button onClick={connectWallet}>Metamask</button>
+        <h3>Connected {walletAddress}</h3>
       </header>
     </div>
   );
