9fe7a2dd7e2e5e6361a0d3d121aed27c3a023071
Aleksei <kilroygood@gmail.com>
2022-05-01 14:32:08 +0300

    didNotWork;(

--- salaryapp/src/App.js
+++ salaryapp/src/App.js
@@ -8,27 +8,28 @@ function App() {
     const contractAddr = '0xBEc49fA140aCaA83533fB00A2BB19bDdd0290f25';
 
     const [defaultAccount, setDefaultAccount] = useState(null);
-    const [walletAddress, setWalletAddress] = useState("");
+    // const [walletAddress, setWalletAddress] = useState("");
+    const [currentContractVal, setCurrentContractVal] = useState(null);
 
     const [provider, setProvider] = useState(null);
-    const [singer, setSinger] = useState(null);
+    const [signer, setSigner] = useState(null);
     const [contract, setContract] = useState(null);
 
     async function connectWalletHandler() {
-    if(window.ethereum){
-      console.log("1")
-      window.ethereum.request({method: "eth_requestAccounts"})
-      .then(result => {
-        accountChanged(result[0])
-        
-      });
-        alert("connected")
-    } else {
-      console.log("error");
-    }
+    if (window.ethereum && window.ethereum.isMetaMask) {
+
+			window.ethereum.request({ method: 'eth_requestAccounts'})
+			.then(result => {
+				accountChangedHandler(result[0]);
+			})
+
+		} else {
+			console.log('Need to install MetaMask');
+		}
+
   }
 
-    const accountChanged = (newAccount) => {
+    const accountChangedHandler = (newAccount) => {
       setDefaultAccount(newAccount);
       updateEthers();
   }
@@ -37,22 +38,40 @@ function App() {
       let tempProvider = new ethers.providers.Web3Provider(window.ethereum);
       setProvider(tempProvider);
 
-      let tempSinger = tempProvider.getSigner();
-      setSinger(tempSinger);
+      let tempSigner = tempProvider.getSigner();
+      setSigner(tempSigner);
 
-      let tempContract = new ethers.Contract(contractAddr, contractABI ,tempSinger);
+      let tempContract = new ethers.Contract(contractAddr, contractABI ,tempSigner);
       setContract(tempContract);
     }
 
+    const setHandler = (event) => {
+      event.preventDefault();
+      console.log(event.target.setVal.value);
+      contract.set(event.target.setVal.value);
+    }
 
+    const chackBal = async () =>{
+      let val = await contract.chackEther();
+      setCurrentContractVal(val);
+    }
   
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={connectWalletHandler}>Metamask</button>
-        <h3>Address {defaultAccount}</h3>
+        <div>
+          <button onClick={connectWalletHandler}>Metamask</button>
+          <h3>Address {defaultAccount}</h3>
+        </div>
+        <form onSubmit={setHandler}>
+          <button type={"submit"}>Check current balance</button>
+          <h3 id='setVal'>Balanse</h3>
+        </form>
+        <button onClick={chackBal}>Check current balance</button>
       </header>
+      {currentContractVal}
     </div>
   );
 }
