95315516cd840f93a52be4707ee9ee86603331fe
Aleksei <kilroygood@gmail.com>
2022-05-03 16:25:35 +0300

    firstWorks

--- salaryapp/src/App.js
+++ salaryapp/src/App.js
@@ -5,7 +5,7 @@ import contractABI from './contractABI.json';
 
 function App() {
 
-    const contractAddr = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+    const contractAddr = '0x610178dA211FEF7D417bC0e6FeD39F05609AD788';
 
     const [defaultAccount, setDefaultAccount] = useState(null);
     // const [walletAddress, setWalletAddress] = useState("");
